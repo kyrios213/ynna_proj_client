fe85f9716627033525cdd5298271935ab11d13b4
Lance Pidor <kyrios213@gmail.com>
2021-12-06 19:37:52 +0800

    changed cols to md

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
     <v-main class="grey lighten-3">
       <v-container>
         <v-row>
-          <v-col cols="3">
+          <v-col md="3">
             <v-sheet rounded="lg">
               <v-list color="transparent" v-if="user">
                 <v-list-item>
